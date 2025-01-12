f496a31cca0a927aeaee0b01aae8109d2eacb4bb
Emmanuel Ifeanyi <nuel087@gmail.com>
2025-01-11 22:31:48 -0800

    Modified the toDisplay Function to check for decimals. Modified the clear function to set variables to falsy values. Added incomplete operator event listeners.

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ const numbs = document.querySelectorAll(".num");
 const decimal = document.querySelector(".decimal");
 const operators = document.querySelectorAll(".operator");
 
-let operand1 = 0;
+let operand1 = null;
 let operator = "";
-let operand2 = 0;
+let operand2 = null;
 
 function toDisplay(input){
-    if(display.textContent === "0"){
+
+    if(display.textContent === "0" && input.textContent !== "."){
         display.textContent = "";
     }
     display.textContent += input.textContent;
 }
 
 function clear(){
-    display.textContent = `${operand1}`;
+    display.textContent = `0`;
+    operand1 = null;
+    operator = "";
+    operand2 = null;
 }
 
+function changeToIntOrFloat(operand){
+    if(operand.includes(".")){
+        operand = parseFloat(operand);
+    }
+}
+
+function operate(){
+
+}
 
 clearBtn.addEventListener('click', clear);
 decimal.addEventListener('click', () => {
@@ -38,5 +51,8 @@ numbs.forEach(numb => {
 operators.forEach(ope => {
     ope.addEventListener('click', () => {
         operand1 = display.textContent;
+        operator = ope.textContent;
     });
 });
+
+clear();
