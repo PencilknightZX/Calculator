e1456ee5ecdd2c94aa968154e1969d37393e22a6
Emmanuel Ifeanyi <nuel087@gmail.com>
2025-01-12 05:12:08 -0800

    Removed operator class from equalBtn and added functionality to it. Operate function still work in progess but currently displays math expression in the console.

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const operators = document.querySelectorAll(".operator");
 const backBtn = document.querySelector(".backspace");
 const percentBtn = document.querySelector(".percent");
 const signBtn = document.querySelector(".plus-minus");
+const equalBtn = document.querySelector(".equal");
 
 let operand1 = null;
 let operator = "";
 let operand2 = null;
+let clearForOperand2 = false;
 
 function toDisplay(numBtnInput){
+      // Check if we can clear display for operand2
+      if(clearForOperand2) {
+        display.textContent = ""; // Clear the display for new input
+        clearForOperand2 = false;         // Reset the flag
+    }
+
     /* Checks inital display content to see if it's on standby
        If a decimal is clicked on during standby it keeps the 0 else it overwrites it */
     if(display.textContent === "0" && numBtnInput !== "."){
@@ -25,6 +33,7 @@ function clear(){
     operand1 = null;
     operator = "";
     operand2 = null;
+    clearForOperand2 = false;
 }
 
 function backspace(){
@@ -60,8 +69,9 @@ function changeToIntOrFloat(operand){
     }
 }
 
-function operate(){
-    pass;
+function operate(ope1, operator, ope2){
+    //Assign result to ope1 and clear ope2
+    console.log(ope1,operator,ope2);
 }
 
 clearBtn.addEventListener('click', clear);
@@ -87,9 +97,24 @@ numbs.forEach(numb => {
 
 operators.forEach(ope => {
     ope.addEventListener('click', () => {
-        operand1 = display.textContent;
-        operator = ope.textContent;
+        if(operand1 === null){
+            operand1 = parseFloat(display.textContent);
+            operator = ope.textContent;
+            clearForOperand2 = true;
+        }
+        else if(operator){
+            operand2 = parseFloat(display.textContent);
+            operate(operand1, operator, operand2);
+            operator = ope.textContent;
+        }
     });
+});
+
+equalBtn.addEventListener('click', () => {
+    if(operand1){
+        operand2 = parseFloat(display.textContent);
+        operate(operand1, operator, operand2);
+    }
 });
 
 //Keyboard Inputs
