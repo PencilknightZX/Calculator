bce4935702534404ef98ce4617aeff6d4f9a0292
Emmanuel Ifeanyi <nuel087@gmail.com>
2025-01-11 23:25:28 -0800

    Added functionality to the plus-minus button and percent button

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const display = document.querySelector(".display");
 const clearBtn = document.querySelector(".clear");
 const numbs = document.querySelectorAll(".num");
-const decimal = document.querySelector(".decimal");
+const decimalBtn = document.querySelector(".decimal");
 const operators = document.querySelectorAll(".operator");
+const percentBtn = document.querySelector(".percent");
+const signBtn = document.querySelector(".plus-minus");
 
 let operand1 = null;
 let operator = "";
 let operand2 = null;
 
-function toDisplay(input){
-
-    if(display.textContent === "0" && input.textContent !== "."){
+function toDisplay(numBtnInput){
+    /* Checks inital display content to see if it's on standby
+       If a decimal is clicked on during standby it keeps the 0 else it overwrites it */
+    if(display.textContent === "0" && numBtnInput.textContent !== "."){
         display.textContent = "";
     }
-    display.textContent += input.textContent;
+    display.textContent += numBtnInput.textContent;
 }
 
 function clear(){
@@ -23,20 +26,45 @@ function clear(){
     operand2 = null;
 }
 
+function toPercent(display){
+    let numb = parseFloat(display.textContent);
+    display.textContent = `${numb/100}`;
+}
+
+function changeSign(display){
+    if(display.textContent === '0' || display.textContent === '0.'){
+        return;
+    }
+    else if(display.textContent.includes("-")){
+        display.textContent = display.textContent.replace("-","");
+        return;
+    }
+    display.textContent = `-${display.textContent}`;
+}
+
 function changeToIntOrFloat(operand){
+    //Inprogress
     if(operand.includes(".")){
         operand = parseFloat(operand);
     }
 }
 
 function operate(){
-
+    pass;
 }
 
 clearBtn.addEventListener('click', clear);
-decimal.addEventListener('click', () => {
-    if(!display.textContent.includes(decimal.textContent)){
-        toDisplay(decimal);
+signBtn.addEventListener('click', () => {
+    changeSign(display);
+});
+
+percentBtn.addEventListener('click', () => {
+    toPercent(display);
+});
+
+decimalBtn.addEventListener('click', () => {
+    if(!display.textContent.includes(decimalBtn.textContent)){
+        toDisplay(decimalBtn);
     }
 });
 
